156e1b9f60c708b453b9df551678bdf6f3c51a83
Ananya Mitra <ananyamitra454@gmail.com>
2025-07-16 17:52:39 +0530

    Add Project  Files

---
+++ dashboard/src/components/WatchList.js
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
+    },
+    title: {
+      display: true,
+      text: "Holdings",
+    },
+  },
+};
+
+export function VerticalGraph({ data }) {
+  return <Bar options={options} data={data} />;
+}
+const WatchList = () => {
+  return (
+    <div>
+      {/* <h3>WatchList Section</h3> */}
+      {/* You can optionally include the graph here later */}
+    </div>
+  );
+};
+
+export default WatchList;
